9f194100d5fc03d59fa777975ffcb48c0d80bc89
Guuuuh <gustavo.htl@hotmail.com>
2022-10-26 10:59:40 -0300

    função para o usuário selecionar as torres
    função para selecionar a torre que terá o disco retirado, e a torre que receberá o disco, não está funcionando ainda

--- Hanoi.py
+++ Hanoi.py
@@ -6,14 +6,13 @@ class Hanoi:
     def __int__(self):
         pass
 
-
     def transfere_disco_de_torre(self, torre_origem, torre_destino):
         if torre_origem.torre_vazia():
             return -1
 
         disco_armazenado = Torre.desempilha_disco(torre_origem)
         disco_tamanho = Disco.get_tamanho(disco_armazenado)
-        primeiro_disco_torre_origem = Disco.get_tamanho(Torre.primeiro_disco_da_torre(torre_origem))
+        primeiro_disco_torre_destino = Disco.get_tamanho(Torre.primeiro_disco_da_torre(torre_destino))
 
-        if disco_tamanho < primeiro_disco_torre_origem:
+        if disco_tamanho < primeiro_disco_torre_destino or Torre.torre_vazia(torre_destino):
             torre_destino.empilha_disco(disco_armazenado)
--- main.py
+++ main.py
@@ -6,10 +6,12 @@ torre1 = Torre(1)
 torre2 = Torre(2)
 torre3 = Torre(3)
 
+hanoi = Hanoi
+
 
 def menu():
-    print("Bem Vindo ao Jogo Torre de Hanoi!!!")
-    quantidade_discos = int(input("Com quantos discos deseja jogar? (min:3 max:8)"))
+    print("Bem-Vindo ao Jogo Torre de Hanoi!!!")
+    quantidade_discos = int(input("Com quantos discos deseja jogar? (min:3 max:8): "))
     if quantidade_discos < 3 or quantidade_discos > 8:
         print("Quantidade inválida, favor inserir a quantidade correta para jogar!")
 
@@ -17,6 +19,31 @@ def menu():
         torre1.empilha_disco(Disco(i))
 
     torre1.exibe_discos_torre()
+    seleciona_torre()
+
+
+def seleciona_torre():
+    torre_origem = int(input("Selecione qual torre deseja retirar o disco: "))
+    torre_destino = int(input("Selecione qual torre deseja colocar o disco: "))
+    if torre_origem == 1:
+        torre_origem = torre1
+    elif torre_origem == 2:
+        torre_origem = torre2
+    elif torre_origem == 3:
+        torre_origem = torre3
+    else:
+        print("Torre inválida! insira 1, 2 ou 3 para selecionar a torre")
+
+    if torre_destino == 1:
+        torre_destino = torre1
+    elif torre_destino == 2:
+        torre_destino = torre2
+    elif torre_destino == 3:
+        torre_destino = torre3
+    else:
+        print("Torre inválida! insira 1, 2 ou 3 para selecionar a torre")
+
+    hanoi.transfere_disco_de_torre(torre_origem, torre_destino)
 
 
 if __name__ == '__main__':
